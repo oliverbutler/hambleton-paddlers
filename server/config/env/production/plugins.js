77a5b8a4511ddc69c0c2e0ddb60e43d788bf0ad5
Oliver Butler <dev@oliverbutler.uk>
2020-09-18 17:52:53 +0100

    🚀 Added AWS S3 support

--- server/config/env/production/plugins.js
+++ server/config/env/production/plugins.js
@@ -17,4 +17,15 @@ module.exports = ({ env }) => ({
       defaultReplyTo: env("EMAIL_REPLY_TO"),
     },
   },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET"),
+      },
+    },
+  },
 });
