c38e14f90cf3478abcd7f7884df0d4b549a67715
Oliver Butler <dev@oliverbutler.uk>
2020-09-18 15:28:06 +0100

    🚀 Heroku deploy attempt

--- server/config/database.js
+++ server/config/database.js
@@ -4,16 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "server"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        uri: "${process.env.DATABASE_URI}",
+        database: "${process.env.DATABASE_NAME}",
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", false),
+        ssl: true,
       },
     },
   },
